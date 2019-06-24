44034329b54bb2368cff38ac8a3c32c62ce58382
vibbulan_519at19 <m.vibbulan@agriya.in>
2019-06-24 13:36:28 +0530

    fixes #1 : Unwanted code has removed.

--- app/Config/cms_bootstrap.php
+++ app/Config/cms_bootstrap.php
@@ -80,7 +80,6 @@ function _pluginBuilder($plugin){
 	$pluginName = Inflector::camelize($plugin);
     if (!file_exists(APP . 'Plugin' . DS . $pluginName)) {
         CakeLog::write(LOG_ERR, 'Plugin not found during bootstrap: ' . $pluginName);
-        continue;
     }
     $bootstrapFile = APP . 'Plugin' . DS . $pluginName . DS . 'Config' . DS . 'bootstrap.php';
     $bootstrap = file_exists($bootstrapFile);
